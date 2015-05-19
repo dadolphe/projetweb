41699e3b62d8965afca1441c80b0d6136eb4a2b7
Masamune0 <Masamune0@users.noreply.github.com>
2015-05-19 17:46:34 +0200

    fixmailreport

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150518191255) do
+ActiveRecord::Schema.define(version: 20150519144843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,10 @@ ActiveRecord::Schema.define(version: 20150518191255) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "pic_file_name"
+    t.string   "pic_content_type"
+    t.integer  "pic_file_size"
+    t.datetime "pic_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
