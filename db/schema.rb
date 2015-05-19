6fb873b061b0fe9ace05c01e1632d2c051f8658e
dadolphe <dadolphe@outlook.fr>
2015-05-19 16:07:19 +0200

    yop

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,11 @@ ActiveRecord::Schema.define(version: 20150513144807) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "firstname"
+    t.integer  "promo"
+    t.string   "mail"
+    t.integer  "phonenumber"
+    t.string   "avatar"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
