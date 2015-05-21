6b73fb7dcdcdde91d39b052a337d81c3c51b065f
Masamune0 <Masamune0@users.noreply.github.com>
2015-05-21 12:39:45 +0200

    up

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
    
-    users = User.create!( [ :email => 'mathieu@yopmail.com', :firstname => 'Mathieu', :name => 'Ber', :promo => '2017', :phonenumber => '0101010101', :password => 'testtest', :admin => 'true' ])
-    users = User.create!( [ :email => 'dylan@yopmail.com', :firstname => 'Dylan', :name => 'Adol', :promo => '2012', :phonenumber => '5101010105', :password => 'testtest', :admin => 'true' ])
-    users = User.create!( [ :email => 'test@yopmail.com', :firstname => 'Test', :name => 'Testname', :promo => '2014', :phonenumber => '0123010101', :password => 'testtest' ])
-    users = User.create!( [ :email => 'marie2@yopmail.com', :firstname => 'Marie', :name => 'Mariename', :promo => '2015', :phonenumber => '0142010101', :password => 'testtest' ])
-    users = User.create!( [ :email => 'pierre56@yopmail.com', :firstname => 'Pierre', :name => 'Pierrename', :promo => '2016', :phonenumber => '0105010101', :password => 'testtest' ])
+users = User.create!( [ :email => 'mathieu@yopmail.com', :firstname => 'Mathieu', :name => 'Ber', :promo => '2017', :phonenumber => '0101010101', :password => 'testtest', :admin => 'true' ])
+users = User.create!( [ :email => 'dylan@yopmail.com', :firstname => 'Dylan', :name => 'Adol', :promo => '2012', :phonenumber => '5101010105', :password => 'testtest', :admin => 'true' ])
+users = User.create!( [ :email => 'test@yopmail.com', :firstname => 'Test', :name => 'Testname', :promo => '2014', :phonenumber => '0123010101', :password => 'testtest' ])
+users = User.create!( [ :email => 'marie2@yopmail.com', :firstname => 'Marie', :name => 'Mariename', :promo => '2015', :phonenumber => '0142010101', :password => 'testtest' ])
+users = User.create!( [ :email => 'pierre56@yopmail.com', :firstname => 'Pierre', :name => 'Pierrename', :promo => '2016', :phonenumber => '0105010101', :password => 'testtest' ])
+
+articles = Article.create!( [:title=> 'FirstArticle', :price=> '500', :user_id => '1', ])
 
 
