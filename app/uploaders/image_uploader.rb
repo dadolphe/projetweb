7714506679acefc57191f4bafe8535472d063b4f
Masamune0 <Masamune0@users.noreply.github.com>
2015-05-21 18:45:41 +0200

    ameliorationvisioneuse

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -33,7 +33,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
    version :thumb do
-     process :resize_to_fit => [50, 50]
+     process :resize_to_fit => [64, 64]
    end
 
    version :small do
