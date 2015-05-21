4794dc913a8d3d48a1d97f037c9af3bc2bb2fcde
Masamune0 <Masamune0@users.noreply.github.com>
2015-05-21 19:05:04 +0200

    seedareReady

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,28 @@ users = User.create!( [ :email => 'test@yopmail.com', :firstname => 'Test', :nam
 users = User.create!( [ :email => 'marie2@yopmail.com', :firstname => 'Marie', :name => 'Mariename', :promo => '2015', :phonenumber => '0142010101', :password => 'testtest' ])
 users = User.create!( [ :email => 'pierre56@yopmail.com', :firstname => 'Pierre', :name => 'Pierrename', :promo => '2016', :phonenumber => '0105010101', :password => 'testtest' ])
 
-articles = Article.create!( [:title=> 'FirstArticle', :price=> '500', :user_id => '1', ])
+articles = Article.create!( [:title=> 'FirstArticle', 'body'=> 'Je suis dans le corps de l''larticle', :price=> '500', :user_id => '1', :created_at => '2015-05-10 16:00:00.41676', :updated_at => '2015-05-10 16:00:00.41676'])
 
+
+articles = Article.create!( [:title=> 'Omni', 'body'=> 'Surrender to the Omniscience.', :price=> '6000', :user_id => '2', :created_at => '2015-04-22 16:00:00.41676', :updated_at => '2015-04-22 16:00:00.41676'])
+
+
+articles = Article.create!( [:title=> 'LeSecond', 'body'=> ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam at quam sodales, egestas dolor nec, efficitur ipsum. Nulla eget urna sagittis, pulvinar leo in, aliquet nulla. Quisque posuere massa nunc, luctus eleifend dolor mattis ut. Pellentesque vestibulum magna lorem, vitae commodo ex rhoncus id. Nunc placerat interdum gravida. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed vitae efficitur purus. Vestibulum a consectetur eros. Morbi ullamcorper ex quis ex tincidunt, in suscipit lectus cursus. Proin a laoreet purus. Donec dapibus sem sed vulputate venenatis. Aliquam scelerisque fermentum diam, semper commodo erat commodo sed. Phasellus vulputate dapibus tempus. Nunc ac faucibus diam. Donec ullamcorper vitae libero ut efficitur.
+
+Ut arcu risus, tempor non convallis a, dignissim vitae turpis. Morbi euismod nunc vel pharetra sodales. Praesent vel varius tortor, sit amet pretium velit. Ut eu tempor risus, vitae semper libero. Nam nisl eros, fringilla nec egestas ut, lacinia id ex. Integer convallis quis erat in gravida. Nunc ac urna vitae leo fringilla gravida eget lacinia dolor. Suspendisse faucibus varius odio, id condimentum ante feugiat rutrum. Nullam vitae sem ac risus consectetur gravida. Aenean eu magna nulla. Sed posuere eget tellus vitae convallis. ', :price=> '30', :user_id => '3', :created_at => '2015-05-15 16:00:00.41676', :updated_at => '2015-05-15 16:00:00.41676'])
+
+
+articles = Article.create!( [:title=> 'The truth', 'body'=> 'Doto is doto, agar is agar.', :price=> '1500', :user_id => '4', :created_at => '2015-05-20 16:00:00.41676', :updated_at => '2015-05-20 16:00:00.41676'])
+
+
+articles = Article.create!( [:title=> 'Strong omni', 'body'=> 'I am ashamed before the Omniscience. ', :price=> '700', :user_id => '4', :created_at => '2015-05-21 16:00:00.41676', :updated_at => '2015-05-21 16:00:00.41676'])
+
+articles = Article.create!( [:title=> 'Strong omni', 'body'=> 'I am ashamed before the Omniscience. ', :price=> '700', :user_id => '5', :created_at => '2015-05-21 16:00:00.41676', :updated_at => '2015-05-21 16:00:00.41676'])
+
+articles = Article.create!( [:title=> 'Leshrac', 'body'=> 'Leshrac the Tormented Soul is an intelligence hero who is known for his destructive spells which can easily destroy Creeps, Heroes and Towers at the expense of Mana. His strong damaging power is balanced by his lack of an escape mechanism, while his only disable, Split Earth, takes time to cast and needs good planning to land successfully.
+Without expert positioning and powerful items, Leshrac is an easy target for physical attackers late-game, but he can easily hamper those same enemies by destroying towers and dealing damage in the midgame. If Leshrac is encountered outside the lanes early on, there are very few opponents who can stand against the power of Diabolic Edict and live.', :price=> '10000', :user_id => '5', :created_at => '2015-05-21 16:00:00.41676', :updated_at => '2015-05-21 16:00:00.41676'])
+
+articles = Article.create!( [:title=> 'Japan', 'body'=> 'Japan and cafe are great.', :price=> '7777', :user_id => '5', :created_at => '2015-05-20 16:00:00.41676', :updated_at => '2015-05-20 16:00:00.41676'])
+
+articles = Article.create!( [:title=> 'Lesh', 'body'=> 'A thousand of your kind have fallen before me.', :price=> '8000', :user_id => '3', :created_at => '2015-05-04 16:00:00.41676', :updated_at => '2015-05-04 16:00:00.41676'])
 
